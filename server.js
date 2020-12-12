2ae846ac533e23b235f89d06241da79911d44d82
LIU Ranxu <liurxdesign@gmail.com>
2020-12-12 23:12:40 +0800

    Update server.js

--- server.js
+++ server.js
@@ -31,12 +31,12 @@ var server = http.createServer(function (request, response) {
         response.end()
     } else if (path === '/style.css') {
         response.statusCode = 200
-        response.setHeader('Content-Type', '/text/css;charset=utf-8')
+        response.setHeader('Content-Type', 'text/css;charset=utf-8')
         response.write(`h1{color: red;}`)
         response.end()
     } else {
         response.statusCode = 404
-        response.setHeader('Content-Type', '/text/html;charset=utf-8')
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
         response.write(`你访问的页面不存在`)
         response.end()
     }
